4ac730a0ef2994e1a0082c1c1b9004bc5c749a3c
Mateus Holanda <mateus23ita@gmail.com>
2023-08-06 12:30:36 -0300

    Set production Backend URL

--- web/src/config/config.js
+++ web/src/config/config.js
@@ -3,6 +3,6 @@ export const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://shirt-customizer-ai.onrender.com/api/v1/dalle",
   },
 }
